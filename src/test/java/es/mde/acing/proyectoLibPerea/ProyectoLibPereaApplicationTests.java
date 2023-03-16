ed7b87fda3841db567ef693f753772ceb11692e8
SurfaceTens <sergio_perea_91@hotmail.com>
2023-03-16 12:53:19 +0100

    Commit inicial

---
+++ src/test/java/es/mde/acing/proyectoLibPerea/ProyectoLibPereaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.mde.acing.proyectoLibPerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLibPereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
